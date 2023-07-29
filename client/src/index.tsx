e17fac2bf9256ca20fee2af993f94388496dc398
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2023-07-29 16:19:57 +0300

    client v1

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
